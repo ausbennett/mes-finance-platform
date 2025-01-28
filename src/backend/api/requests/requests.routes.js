686f7c52eb2b769f3b5f3e9ae89655bdf080b391
Austin Bennett <ausbenn@icloud.com>
2025-01-28 13:07:53 -0500

    Initialize Plaid Routes in Requests Router

--- src/backend/api/requests/requests.routes.js
+++ src/backend/api/requests/requests.routes.js
@@ -2,6 +2,7 @@
 /* REQUESTS ROUTER
  * This is where we define all the routes (endpoints) for the requests, 
  * the logic that processes the data is in the corrisponding controller module 
+ * this file should look pretty simple, because this should just be a 1:1 pointer to the requestsController
 */
 
 const express = require('express');
@@ -9,9 +10,18 @@ const router = express.Router();
 
 const requestsController = require('./requests.controller');
 
-// Route definitions
+// Route definitions (place holder)
 router.get('/', requestsController.getAllRequests);
 router.post('/', requestsController.createRequest);
 router.get('/:id', requestsController.getRequestById);
+
+// Plaid Specific Routes
+
+// body.userID needed for this
+router.post('/plaid/link-token', requestsController.plaidCreateLinkToken);
+
+router.post('/plaid/exchange-token', requestsController.plaidExchangePublicToken);
+router.get('/plaid/transactions', requestsController.plaidGetTransactions);
+
 
 module.exports = router;
--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,23 @@
-//THIS IS THE MAIN ENTRY POINT
+/* APP
+ * MAIN EXPRESS APP, highest, most abstract level of business logic
+ * */
 
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
+//routers
+const requestsRouter = require('./api/requests/requests.routes');
 
+// Middleware (if needed)
+app.use(express.json());
+
+// Use the requests router for the /api/requests.routes path
+app.use('/api/requests', requestsRouter);
+
+// Base route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+// Export the app
 module.exports = app

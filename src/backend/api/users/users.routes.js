06a6c87698ce971b2607bc85d01f087a6a5741d5
Austin Bennett <ausbenn@icloud.com>
2025-02-09 15:52:08 -0500

    Added getMe to get the current user (for frontend use)

--- src/backend/api/users/users.routes.js
+++ src/backend/api/users/users.routes.js
@@ -4,6 +4,7 @@ const userController = require('./users.controller');
 const router = express.Router();
 
 router.get('/', userController.getUsers);         // GET all users (Admin only)
+router.get('/me', userController.getMe)           //Get Current User
 router.get('/:id', userController.getUserById);   // GET user by ID
 router.post('/', userController.createUser);      // CREATE user
 router.put('/:id', userController.updateUser);    // UPDATE user

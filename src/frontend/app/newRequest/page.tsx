879a1b83186c2bd7c272b560f9c770e363371c08
adampodolak <podola2@mcmaster.ca>
2025-03-29 13:50:57 -0400

    getting rid of budget line dropdown

--- src/frontend/app/newRequest/page.tsx
+++ src/frontend/app/newRequest/page.tsx
@@ -196,83 +196,77 @@ export default function NewRequestPage() {
          <div className="flex flex-col min-h-screen min-w-screen bg-gray-200 space-y-10">
             <NavBar />
 
-        <div className="flex flex-col items-center justify-center flex-grow">
-          <div className="flex flex-col w-2/3 flex-grow space-y-5">
-            <div className="flex flex-row items-center justify-start ">
-              <p className="text-primary-text font-bold text-lg">
-                Request Form
-              </p>
-            </div>
-            <div className="flex flex-col items-center justify-start flex-grow bg-foreground p-10 rounded-xl shadow-md space-y-10">
-              <div className="flex flex-row items-center justify-between space-x-5">
-                <div className="flex flex-row items-start justify-center space-x-2">
-                  <div className="flex flex-col items-start justify-center pt-1">
-                    <input
-                      type="radio"
-                      value="reimbursement"
-                      checked={radio == "reimbursement"}
-                      onChange={() => handleRadioSelect("reimbursement")}
-                    />
+            <div className="flex flex-col items-center justify-center flex-grow">
+               <div className="flex flex-col w-2/3 flex-grow space-y-5">
+                  <div className="flex flex-row items-center justify-start ">
+                     <p className="text-primary-text font-bold text-lg">
+                        Request Form
+                     </p>
                   </div>
-                  <div className="flex flex-col items-start justify-center">
-                    <p className="font-bold">Reimbursement Request</p>
-                    <p>
-                      You have already paid for something, and would
-                      like the MES to pay you back
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-row items-start justify-center space-x-2">
-                  <div className="flex flex-col items-start justify-center pt-1">
-                    <input
-                      type="radio"
-                      value="payment"
-                      checked={radio == "payment"}
-                      onChange={() => handleRadioSelect("payment")}
-                    />
-                  </div>
-                  <div className="flex flex-col items-start justify-center">
-                    <p className="font-bold">Payment Request</p>
-                    <p>
-                      You are requesting the MES pay someone on your
-                      behalf
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-center w-1/3 space-y-2">
-                  <p className="font-bold">Budget Line</p>
-                  <select className="bg-foreground px-3 py-2 rounded-md w-full border-white drop-shadow-md cursor-pointer">
-                    <option>option1</option>
-                    <option>option1</option>
-                    <option>option1</option>
-                  </select>
-                </div>
-              </div>
+                  <div className="flex flex-col items-center justify-start flex-grow bg-foreground p-10 rounded-xl shadow-md space-y-10">
+                     <div className="flex flex-row items-center justify-between space-x-5">
+                        <div className="flex flex-row items-start justify-center space-x-2">
+                           <div className="flex flex-col items-start justify-center pt-1">
+                              <input
+                                 type="radio"
+                                 value="reimbursement"
+                                 checked={radio == "reimbursement"}
+                                 onChange={() =>
+                                    handleRadioSelect("reimbursement")
+                                 }
+                              />
+                           </div>
+                           <div className="flex flex-col items-start justify-center">
+                              <p className="font-bold">Reimbursement Request</p>
+                              <p>
+                                 You have already paid for something, and would
+                                 like the MES to pay you back
+                              </p>
+                           </div>
+                        </div>
+                        <div className="flex flex-row items-start justify-center space-x-2">
+                           <div className="flex flex-col items-start justify-center pt-1">
+                              <input
+                                 type="radio"
+                                 value="payment"
+                                 checked={radio == "payment"}
+                                 onChange={() => handleRadioSelect("payment")}
+                              />
+                           </div>
+                           <div className="flex flex-col items-start justify-center">
+                              <p className="font-bold">Payment Request</p>
+                              <p>
+                                 You are requesting the MES pay someone on your
+                                 behalf
+                              </p>
+                           </div>
+                        </div>
+                     </div>
 
-              {/* FORM START */}
-              <div className="flex flex-col flex-grow w-full bg-foreground rounded-xl shadow-lg border border-tertiary p-10 space-y-5">
-                {radio === "payment" && (
-                  <PaymentRequest 
-                    user={{
-                      ...user,
-                      displayName: getDisplayName(email),
-                      clubName: paymentFormData.club
-                    }} 
-                    formData={paymentFormData} 
-                    setFormData={setPaymentFormData} 
-                  />
-                )}
-                {radio === "reimbursement" && (
-                  <ReimbursementRequest 
-                    user={{
-                      ...user,
-                      displayName: getDisplayName(email),
-                      clubName: reimbursementFormData.club
-                    }} 
-                    formData={reimbursementFormData} 
-                    setFormData={setReimbursementFormData} 
-                  />
-                )}
+                     {/* FORM START */}
+                     <div className="flex flex-col flex-grow w-full bg-foreground rounded-xl shadow-lg border border-tertiary p-10 space-y-5">
+                        {radio === "payment" && (
+                           <PaymentRequest
+                              user={{
+                                 ...user,
+                                 displayName: getDisplayName(email),
+                                 clubName: paymentFormData.club,
+                              }}
+                              formData={paymentFormData}
+                              setFormData={setPaymentFormData}
+                           />
+                        )}
+                        {radio === "reimbursement" && (
+                           <ReimbursementRequest
+                              user={{
+                                 ...user,
+                                 displayName: getDisplayName(email),
+                                 clubName: reimbursementFormData.club,
+                              }}
+                              formData={reimbursementFormData}
+                              setFormData={setReimbursementFormData}
+                           />
+                        )}
 
                         <div className="flex flex-row items-center justify-end">
                            <button
